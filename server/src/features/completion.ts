e1fa2d53f1c00edd38a187857f0235b9d5a3bb65
mrmld <die.marmelade@yandex.ru>
2022-04-06 00:56:26 +0300

    wip: trigger completion for dot and tilda

--- server/src/features/completion.ts
+++ server/src/features/completion.ts
@@ -7,7 +7,10 @@ export class CompletionItemProvider {
     constructor(private readonly _documents: DocumentStore, private readonly _trees: Trees, private readonly _symbols: SymbolIndex) {}
 
     register(connection: lsp.Connection) {
-        connection.client.register(lsp.CompletionRequest.type);
+        connection.client.register(lsp.CompletionRequest.type, {
+            documentSelector: [{ language: 'func' }],
+            triggerCharacters: ['.', '~']
+        });
         connection.onRequest(lsp.CompletionRequest.type, this.provideCompletionItems.bind(this));
     }
 
@@ -18,42 +21,50 @@ export class CompletionItemProvider {
 			return [];
 		}
 
+        let onlyFunctions = false;
+        if (params.context?.triggerCharacter === '.' || params.context?.triggerCharacter === '~') {
+            onlyFunctions = true;
+        }
+
         let result: lsp.CompletionItem[] = [];
 
         // local symbols
-        let cursorPosition = {
-            column: params.position.character,
-            row: params.position.line,
-        }
-        let descendant = tree.rootNode.descendantForPosition(cursorPosition);
-
-        // navigate through parents and find their variables declared higher than cursor
-        while (descendant) {
-            while (descendant && descendant.type !== 'block_statement') {
-                descendant = descendant.parent;
+        if (!onlyFunctions) {
+            let cursorPosition = {
+                column: params.position.character,
+                row: params.position.line,
             }
-            if (!descendant) {
-                continue;
-            }
-            for (let child of descendant.children) {
-                if (child.type == 'expression_statement') {
-                    let variableDeclarations = child.descendantsOfType('variable_declaration', null, cursorPosition);
-                    for (let varDec of variableDeclarations) {
-                        let identifiers = varDec.descendantsOfType('identifier', null, cursorPosition);
-                        result.push(...identifiers.map(a => {
-                            let item = lsp.CompletionItem.create(a.text);
-                            item.kind = lsp.CompletionItemKind.Variable;
-                            return item;
-                        }))
+            let descendant = tree.rootNode.descendantForPosition(cursorPosition);
+    
+            // navigate through parents and find their variables declared higher than cursor
+            while (descendant) {
+                while (descendant && descendant.type !== 'block_statement') {
+                    descendant = descendant.parent;
+                }
+                if (!descendant) {
+                    continue;
+                }
+                for (let child of descendant.children) {
+                    if (child.type == 'expression_statement') {
+                        let variableDeclarations = child.descendantsOfType('variable_declaration', null, cursorPosition);
+                        for (let varDec of variableDeclarations) {
+                            let identifiers = varDec.descendantsOfType('identifier', null, cursorPosition);
+                            result.push(...identifiers.map(a => {
+                                let item = lsp.CompletionItem.create(a.text);
+                                item.kind = lsp.CompletionItemKind.Variable;
+                                return item;
+                            }))
+                        }
                     }
                 }
+                descendant = descendant.parent;
             }
-            descendant = descendant.parent;
         }
 
         // global symbols
+        await this._symbols.update();
         let symbols = new Set<string>();
-        for (let [label, occurencies] of this._symbols.index.query('')) {
+        for (let [label, occurencies] of this._symbols.index) {
             for (let [doc, symbol] of occurencies.entries()) {
                 if (symbol.definitions.size === 0) {
                     continue;
@@ -66,7 +77,7 @@ export class CompletionItemProvider {
                     let item = lsp.CompletionItem.create(label);
                     if (def === lsp.SymbolKind.Function) {
                         item.kind = lsp.CompletionItemKind.Function;
-                    } else if (def === lsp.SymbolKind.Variable) {
+                    } else if (def === lsp.SymbolKind.Variable && !onlyFunctions) {
                         item.kind = lsp.CompletionItemKind.Variable;
                     } else {
                         continue;
@@ -75,6 +86,7 @@ export class CompletionItemProvider {
                 }
             }
         }
+
         return result;
     }
 }
